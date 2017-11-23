1e21e56ecce00750ae65b237914db4fbd0b5eb1b
Florian ROMEO <florian.romeo@feragon.net>
2017-11-23 16:58:45 +0100

    Validité des formes

--- formes/cercle.cpp
+++ formes/cercle.cpp
@@ -1,12 +1,15 @@
 #include "cercle.h"
 #include <const.h>
 #include <math.h>
+#include <exceptions/argumentexception.h>
 
 Cercle::Cercle(const char* couleur, const Vecteur& centre, double rayon) :
     Forme(couleur),
     _centre(centre),
     _rayon(rayon) {
-
+    if(_rayon <= 0) {
+        throw ArgumentException("Le rayon est null");
+    }
 }
 
 void Cercle::setCentre(const Vecteur& centre) {
